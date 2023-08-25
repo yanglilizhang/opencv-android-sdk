dea4331e6dc1d7e648ee1e2457f834704073b480
zhangw <zhangw@uchoice.cc>
2023-08-25 11:09:19 +0800

    test

--- app/src/main/java/com/lib/opencv_android_sdk/MainActivity.kt
+++ app/src/main/java/com/lib/opencv_android_sdk/MainActivity.kt
@@ -2,7 +2,7 @@ package com.lib.opencv_android_sdk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//ss
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
